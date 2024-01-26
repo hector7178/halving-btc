26442541972d9f87250b35610997eadec41afabf
hector7178 <hjgrimanmarcano@gmail.com>
2024-01-26 19:57:36 -0400

    first commit

---
+++ app/components/Crono.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+const CountdownTimer = (prop) => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  // Fecha objetivo
+  const endDate = new Date("09 April 2024, 21:08 UTC");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const timeLeft = endDate - now;
+
+      // Calcula días restantes
+      const remainingDays = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      setDays(remainingDays);
+
+      // Calcula horas restantes
+      const remainingHours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      setHours(remainingHours);
+
+      // Calcula minutos restantes
+      const remainingMinutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(remainingMinutes);
+
+      // Calcula segundos restantes
+      const remainingSeconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+      setSeconds(remainingSeconds);
+
+      // Limpia el intervalo cuando el tiempo se agota
+      if (timeLeft <= 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div {...prop}>
+        <h4 className="text-white text-4xl w-full text-center font-bold">COUNTDOWN</h4>
+        <div className="grid grid-cols-4 h-fit gap-2">
+        <p className="rounded-lg bg-white text-amber-500 flex flex-col  items-center justify-center shadow-lg text-2xl md:text-5xl py-10">{days}:<br/><span className="text-sm">Days</span></p>
+        <p className="rounded-lg bg-white text-amber-500 flex flex-col  items-center justify-center shadow-lg text-2xl md:text-5xl py-10">{hours}:<br/><span className="text-sm">Hours</span></p>
+        <p className="rounded-lg bg-white text-amber-500 flex  flex-col items-center justify-center shadow-lg text-2xl md:text-5xl py-10">{minutes}:<br/><span className="text-sm">Minutes</span></p>
+        <p className="rounded-lg bg-white text-amber-500 flex  flex-col items-center justify-center shadow-lg text-2xl md:text-5xl py-10">{seconds}<br/><span className="text-sm">Seconds</span></p>
+        </div>
+        <span className="text-white text-xl w-full text-center font-bold">09 April 2024, 21:08 UTC</span>
+    </div>
+  );
+};
+
+export default CountdownTimer;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,76 @@
+'use client'
+
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { useMotionValueEvent, useScroll } from 'framer-motion'
+
+function Navbar(prop) {
+  const [open , setOPen ]=useState(false)
+  const { scrollY } = useScroll()
+  const [scroll,setScroll]=useState(0)
+  const [change,setChange]=useState(true)
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setScroll(latest)
+    if(change){
+      setChange(false)
+    }
+   
+  })
+
+  useEffect(()=>{
+      setTimeout(()=>{
+        setChange(true)
+      },7000)
+  },[change])
+ 
+
+  return (
+  <>
+   
+    <header className={(scroll>65?(!change?' fixed translate-y-0 origin-top transition-all duration-1000 ease-out ':' md:bg-orange-50 origin-top transition-all duration-1000 ease-out fixed md:-translate-y-full hover:translate-y-0 '):' bg-transparent md:bg-orange-50 fixed md:relative ')+'  w-full h-[11vh] grid grid-cols-5 z-20'}>
+      <div className='col-span-2'>
+
+      </div>
+      <div className='col-span-3 flex justify-end '>
+        <ul className={(open?'flex flex-col md:flex-row  pt-6  absolute md:static z-20 right-0 scale-100':'flex  flex-col absolute  md:static top-0 right-0 md:flex-row gap-2 scale-0 md:scale-100')+' rounded-bl-lg bg-orange-50 md:bg-transparent h-fit w-fit md:justify-end origin-top-right transition-all duration-300 ease-in-out gap-6 p-4'}>
+          <li className='w-full flex items-end justify-end md:hidden' onClick={()=>setOPen(false)}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+
+          </li>
+          <li>
+            <Link href={'/#home'} className=' text-yellow-950 text-lg flex-row flex items-center gap-2 hover:border-b-2 rounded-lg hover:border-amber-500  p-2 ease-in-out duration-150 transition-all'>
+              
+              <span>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/#about'} className=' text-yellow-950 text-lg flex-row flex items-center gap-2 hover:border-b-2 rounded-lg hover:border-amber-500  p-2 ease-in-out duration-150 transition-all'>
+           
+              <span>How It Works?</span>
+            </Link>
+          </li>
+         
+          <li>
+            <Link href={'/'} className=' text-yellow-950 text-lg flex-row flex items-center gap-2 hover:border-b-2 rounded-lg hover:border-amber-500  p-2 ease-in-out duration-150 transition-all' >
+            
+              <span>Countdown</span>
+            </Link>
+          </li>
+        </ul> 
+        
+        <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>setOPen(true)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={(open?"flex scale-0 opacity-0 w-8 h-8 absolute right-6 top-6":"scale-100 opacity-1 flex md:hidden w-8 h-8 absolute right-6 top-6")+' transition-all duration-300 ease-in-out'}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </div>
+    
+    </header>
+   
+  </>
+ 
+  )
+}
+
+export default Navbar
